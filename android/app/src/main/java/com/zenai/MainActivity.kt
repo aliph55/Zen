1060d0dd2207a5bd8cf12a88fdee42b0eded6b78
Ali Pourhassan <67645398+aliph55@users.noreply.github.com>
2025-11-05 22:05:47 +0300

    rename

--- android/app/src/main/java/com/zenai/MainActivity.kt
+++ android/app/src/main/java/com/zenai/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.zen
+package com.zenai
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/zenai/MainApplication.kt
+++ android/app/src/main/java/com/zenai/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.zen
+package com.zenai
 
 import android.app.Application
 import com.facebook.react.PackageList
