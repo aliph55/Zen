35c6e6db2e628607ee26fa4f6486e1d28da70772
Ali Pourhassan <67645398+aliph55@users.noreply.github.com>
2026-01-04 22:08:11 +0300

    react-native-upgrade

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-/**
- * @format
- */
-
 import { AppRegistry } from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+// Buradaki ilk parametre "zenai" olmalı
+AppRegistry.registerComponent('zenai', () => App);
